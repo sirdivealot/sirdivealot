fc9e56b3e6180139b792173765b868ca7295698a
sirdivealot <40392062+sirdivealot@users.noreply.github.com>
2018-06-19 15:35:54 +0200

    webhook test 2

--- src/world/index.js
+++ src/world/index.js
@@ -1,18 +1,6 @@
 const content = `
-	<div class="container">
-		<div class="content">
-			<h1>We Are Live!</h1>
-			<span class="right">2018-06-17</span>
-			<p>Comin' at ya' with nudies and shrimps.</p>
-		</div>
-	</div>
+<p>Someday we will have been everywhere</p>
 `
-module.exports = function(output, dst) {
-	require('partials/header')(output)
-	output.write('<body>')
-	require('partials/jumbo')(output)
-	require('partials/nav')(output, 'World')
-	output.write(content)
-	output.write('</body>')
-	require('partials/footer')(output)
+module.exports = function(output) {
+	require('layout')(output, 'World', 'Dive the World!', content)
 }
