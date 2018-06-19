0a07cd7bf17f82fca924816f7b5c3924ba1a9981
Axel Smeets <murlocbrand@gmail.com>
2018-06-19 16:01:05 +0200

    keep dist folder

--- prod.js
+++ prod.js
@@ -1,7 +1,4 @@
 process.env["NODE_ENV"] = "production"
 
-const fs    = require('fs-extra')
-
-fs.removeSync('dist')
-fs.ensureDirSync('dist')
+require('fs-extra').ensureDirSync('dist')
 require('./build')()
